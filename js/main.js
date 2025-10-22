e453886ab22bebc6c8f9d06803f68fcdb7759c19
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-10-21 19:43:35 -0500

    [$] index.html: Reemplaza estrellas por un input range [+] main.js: Captura el valor del input range y lo muestra en pantalla [+] reviews.css: Vincula CSS separado

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log( 'Bienvenidos a Foodz!' );
+
+const ratingPage = document.querySelector('#ratingRange');
+const ratingValue = document.querySelector('#ratingValue');
+
+ratingPage.addEventListener( 'input', () => {
+    ratingValue.textContent = ratingPage.value;
+} )
+
+console.log( ratingPage, ratingValue );
